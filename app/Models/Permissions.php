d29476d7dc396b947b5830c573229d6aba30b6de
Ferdy Rahmat <ferdyyrahmat@gmail.com>
2024-10-16 10:45:55 +0700

    Update Permissions.php

--- app/Models/Permissions.php
+++ app/Models/Permissions.php
@@ -14,26 +14,27 @@ class Permissions extends Model
     protected $table = 'permission';
 
     protected $fillable = [
+        'id',
         'name',
     ];
 
-    public function getDataPermissionJoin($where = false)
-    {
-        if ($where === false) {
-            $builder = DB::table('permission');
-            $builder->select('*');
-            $builder->leftJoin('role_permission', 'permission.id', '=', 'role_permission.permission_id');
-            $builder->leftJoin('role', 'role.id', '=', 'role_permission.role_id');
-            $builder->orderBy('permission.id', 'ASC');
-            return $query = $builder->get();
-        } else {
-            $builder = DB::table('permission');
-            $builder->select('*');
-            $builder->where($where);
-            $builder->leftJoin('role_permission', 'permission.id', '=', 'role_permission.permission_id');
-            $builder->leftJoin('role', 'role_permission.role_id', '=', 'role.id');
-            $builder->orderBy('permission.id', 'ASC');
-            return $query = $builder->get();
-        }
-    }
+    // public function getDataPermissionJoin($where = false)
+    // {
+    //     if ($where === false) {
+    //         $builder = DB::table('permission');
+    //         $builder->select('*');
+    //         $builder->leftJoin('role_permission', 'permission.id', '=', 'role_permission.permission_id');
+    //         $builder->leftJoin('role', 'role.id', '=', 'role_permission.role_id');
+    //         $builder->orderBy('permission.id', 'ASC');
+    //         return $query = $builder->get();
+    //     } else {
+    //         $builder = DB::table('permission');
+    //         $builder->select('*');
+    //         $builder->where($where);
+    //         $builder->leftJoin('role_permission', 'permission.id', '=', 'role_permission.permission_id');
+    //         $builder->leftJoin('role', 'role_permission.role_id', '=', 'role.id');
+    //         $builder->orderBy('permission.id', 'ASC');
+    //         return $query = $builder->get();
+    //     }
+    // }
 }
