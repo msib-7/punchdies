7bc79b834f0eba27200d9d7c443341910621cbd6
Ferdy Rahmat <ferdyyrahmat@gmail.com>
2024-10-17 13:18:01 +0700

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Permissions;
+use App\Models\Roles;
+use App\Models\RolesPermission;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Str;
@@ -44,16 +46,61 @@ class AdminController extends Controller
     //Start Role
     public function manajemen_role()
     {
+        $modelRole = new Roles();
+
+        $dataRoles = Roles::all();
+        $data['dataRoles'] = $dataRoles;
+
+        $dataRolePermission = $modelRole->getRoleJoinPermission()->all();
+        $data['permissions'] = $dataRolePermission;
+
+
+        // dd($dataRoles);
         $dataPermissions = Permissions::all();
         $permissions_by_group = [];
         foreach ($dataPermissions ?? [] as $permission) {
             $ability = Str::after($permission->name, ' ');
             $permissions_by_group[$ability][] = $permission;
         }
-
+        $data['permissions_by_group'] = $permissions_by_group;
         // dd(compact('permissions_by_group'));
 
-        return view("admin/manajemen-role/data-role", compact('permissions_by_group'));
+        return view("admin/manajemen-role/data-role", $data);
     }
 
+    public function add_role(Request $request)
+    {
+        $role_name = ucwords($request->role_name);
+        $permission_value = $request->permission_value;
+        
+        $saveRole = [
+            'role_name' => $role_name,
+        ];
+
+        $cekRole = Roles::whereLike('role_name', $role_name )->first();
+        // dd(isset($cekPermission));
+        if (!isset($cekRole)) {
+            Roles::create($saveRole);
+        } else {
+            return redirect(route('roles'))->with('error', 'Role sudah ada!');
+        }
+        
+        $dataRoles = Roles::where('role_name', '=', $role_name)
+                            ->orderBy('created_at', 'desc')
+                            ->first();
+        $role_id = $dataRoles['id'];
+
+        if($permission_value != ''){
+            $count = count($permission_value);
+            for ($i=0; $i < $count; $i++) { 
+                $saveRolePermission = [
+                    'permission_id' => $permission_value[$i],
+                    'role_id' => $role_id,
+                ];
+                RolesPermission::create($saveRolePermission);
+            }
+        }
+
+        return redirect(route('roles'))->with('success', 'Role berhasil diBuat!');
+    }
 }
