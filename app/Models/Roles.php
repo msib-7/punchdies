710d85f69565bef683b7e78af0f78c2f00dc9123
Ferdy Rahmat <ferdyyrahmat@gmail.com>
2024-10-17 13:18:19 +0700

    Create Roles.php

---
+++ app/Models/Roles.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use DB;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Roles extends Model
+{
+    use HasFactory;
+
+    protected $table = 'role';
+
+    protected $fillable = [
+        'role_name',
+    ];
+
+    public function getRoleJoinPermission()
+    {
+        $builder = DB::table($this->table);
+        $builder->select('*');
+        $builder->Join('role_permission', 'role.id','=', 'role_permission.role_id');
+        $builder->Join('permission', 'role_permission.permission_id','=', 'permission.id');
+        $builder->orderBy('role.id', 'asc');
+        return $query = $builder->get();
+    }
+}
