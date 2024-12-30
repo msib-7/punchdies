ab0538d31da174998219824843bd1519be2d6274
Technical Support - Dev <ferdyyrahmat@gmail.com>
2024-12-30 09:12:03 +0700

    .

--- resources/views/operator/data/form/pengukuran-dies.blade.php
+++ resources/views/operator/data/form/pengukuran-dies.blade.php
@@ -356,7 +356,7 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-primary">Save changes</button>
+                <button type="submit" class="btn btn-primary">Save changess</button>
             </div>
             </form>
         </div>
