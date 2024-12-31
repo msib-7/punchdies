71380f50ff16a23a0b9116c6e7144c4c19e31852
Technical Support - Dev <ferdyyrahmat@gmail.com>
2024-12-31 09:19:11 +0700

    Fix typo in 'Audit Trial' button label to 'Audit Trail'

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -493,8 +493,8 @@
     <!--end::sidebar menu-->
     <!--begin::Footer-->
     <div class="app-sidebar-footer flex-column-auto pt-2 pb-6 px-6" id="kt_app_sidebar_footer">
-        <a href="{{ route('audit') }}" class="btn btn-flex flex-center btn-custom btn-primary overflow-hidden text-nowrap px-0 h-40px w-100" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-dismiss-="click" title="200+ in-house components and 3rd-party plugins">
-            <span class="btn-label">Audit Trial</span>
+        <a href="{{ route('audit') }}" class="btn btn-flex flex-center btn-custom btn-primary overflow-hidden text-nowrap px-0 h-40px w-100">
+            <span class="btn-label">Audit Trail</span>
             <i class="ki-duotone ki-document btn-icon fs-2 m-0">
                 <span class="path1"></span>
                 <span class="path2"></span>
