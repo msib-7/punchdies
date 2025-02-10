9960f6ad821a0990e59c9330816365fc9ff6740d
Technical Support - Dev <ferdyyrahmat@gmail.com>
2025-02-10 08:36:39 +0700

    Remove commented-out code for clarity in settings form layout

--- resources/views/dev/settings/form/index.blade.php
+++ resources/views/dev/settings/form/index.blade.php
@@ -257,7 +257,7 @@
             </div>
             <div class="col-12 col-md-4">
                 <div class="row">
-                    <div class="col-12">Dies</div>
+                    {{-- <div class="col-12">Dies</div> --}}
                 </div>
             </div>
         </div>
