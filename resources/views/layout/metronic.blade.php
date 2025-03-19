b53dcfd8df3bedd4727101d481ca534ad6172740
Technical Support - Dev <ferdyyrahmat@gmail.com>
2025-03-19 11:49:03 +0700

    Update sidebar toggle logic to minimize on non-dashboard pages

--- resources/views/layout/metronic.blade.php
+++ resources/views/layout/metronic.blade.php
@@ -10,7 +10,7 @@
 <body id="kt_app_body" data-kt-app-layout="light-sidebar" data-kt-app-header-fixed="true"
     data-kt-app-sidebar-enabled="true" data-kt-app-sidebar-fixed="true" data-kt-app-sidebar-hoverable="true"
     data-kt-app-sidebar-push-header="true" data-kt-app-sidebar-push-toolbar="true"
-    <?php if(Request::segment(4) == 'form_pengukuran'){echo 'data-kt-app-sidebar-minimize="on"';}?>
+    <?php if(Request::segment(1) != 'dashboard'){echo 'data-kt-app-sidebar-minimize="on"';}?>
     data-kt-app-sidebar-push-footer="true" data-kt-app-toolbar-enabled="true" class="app-default"
     style="background-image: url('/assets/img/bglineB.svg'); background-repeat: repeat-y;">
 
